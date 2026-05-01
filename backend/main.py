cd950ee26b653cb1b06e00d4b84317b44f3a713e
KEERTHANA K H <keerthanakh01@gmail.com>
2026-05-01 18:27:28 +0530

    Update main.py

--- backend/main.py
+++ backend/main.py
@@ -2,7 +2,7 @@ from fastapi import FastAPI
 app = FastAPI()
 @app.get("/")
 def root():
-    return {"message": "ArogyaAI Backend Running"}
+   return {"message": "ArogyaAI Backend Running"}
 @app.post("/triage")
 def triage(symptoms: list[str]):
     return {"urgency": "moderate", "suggestion": "Consult doctor within 24 hours"}
